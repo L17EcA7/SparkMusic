8b9b1d01e1e44afd4958feec0ab0ddb186f2fb96
im华喵 <littttt87@hotmail.com>
2024-02-06 13:34:30 +0800

    🔮 0.1.0：支持最新api

--- src/renderer/src/js/music.js
+++ src/renderer/src/js/music.js
@@ -10,76 +10,38 @@ const handletoken = (text) => {
     const formattedDateTime = `${year}${month}${day}${text}`;
     return formattedDateTime;
 };
-
 const gettoken = (url, level) => {
     const rawToken = handletoken(url + level + 'musiccn_v1');
     const encryptedToken = CryptoJS.MD5(rawToken).toString();
     return encryptedToken;
 };
 async function _getMusicInfo(ID) {
-    const url = `https://music.163.com/song?id=${ID}`;
-    const level = "jymaster";
-    const _token = gettoken(url, level);
-
-    const requestData = {
-        "url": url,
-        "level": level,
-        "type": "song",
-        "token": _token
-    };
-    const Header = {
-        "Token": _token,
-        "Timestamp": Date.now(),
-    };
-
     try {
-        const response = await axios.post('https://api.toubiec.cn/api/music_v1.php', requestData, {
-            headers: Header
-        });
-        return response.data;
-    } catch (error) {
-        console.error(error.response.data);
-        return error.response.data;
+        const Response = await axios.get(`https://api.injahow.cn/meting/?type=song&id=${ID}`);
+        // 只有在备用接口状态码为200时才返回备用数据
+        if (Response.data.code !== 500) {
+            return Response.data;
+        } else {
+            console.error(`Error fetching playlist info. Status code: ${Response.data.code}`);
+        }
+    } catch (backupError) {
+        console.error('Error fetching playlist info:', backupError);
+        throw new Error('Failed to fetch playlist info');
     }
 }
 async function _getPlaylistInfo(ID) {
-    const maxRetries = 3; // 最大重试次数
-    let currentRetry = 0;
-
-    while (currentRetry < maxRetries) {
-        try {
-            const response = await axios.get(`https://api.baka.fun/netease?type=playlist&id=${ID}`);
-
-            // 只有在状态码为200时才返回数据
-            if (response.data.code !== 500) {
-                return response.data;
-            } else {
-                console.error(`Error fetching playlist info. Status code: ${response.data.code}`);
-            }
-        } catch (error) {
-            console.error('Error fetching playlist info:', error);
-        }
-
-        currentRetry++;
-        console.log(`Retrying (${currentRetry}/${maxRetries})...`);
-    }
-
-    // 如果重试达到最大次数仍然失败，可以尝试备用接口
     try {
-        const backupResponse = await axios.get(`https://api.injahow.cn/meting/?type=playlist&id=${ID}`);
-
+        const Response = await axios.get(`https://api.injahow.cn/meting/?type=playlist&id=${ID}`);
         // 只有在备用接口状态码为200时才返回备用数据
-        if (backupResponse.data.code !== 500) {
-            return backupResponse.data;
+        if (Response.data.code !== 500) {
+            return Response.data;
         } else {
-            console.error(`Error fetching playlist info (backup). Status code: ${backupResponse.data.code}`);
+            console.error(`Error fetching playlist info. Status code: ${Response.data.code}`);
         }
     } catch (backupError) {
-        console.error('Error fetching playlist info (backup):', backupError);
+        console.error('Error fetching playlist info:', backupError);
+        throw new Error('Failed to fetch playlist info');
     }
-
-    // 如果所有尝试都失败，你可以根据需要处理
-    throw new Error('Failed to fetch playlist info');
 }
 
 async function getMusicInfo(ID) {
@@ -88,9 +50,8 @@ async function getMusicInfo(ID) {
 async function getPlaylistInfo(ID) {
     return await _getPlaylistInfo(ID);
 }
-async function GetMusicLink(ID) {
-    const a = await _getMusicInfo(ID);
-    return a.url_info.url;
+function GetMusicLink(ID) {
+    return `https://api.toubiec.cn/api/wyapi_v1.php?id=${ID}&type=urlv1&level=jymaster&cont=1`;
 }
 // 示例用法
 
@@ -98,7 +59,6 @@ async function netease2audio(musicList, batchSize = 300, delayBetweenBatches = 2
     const result = [];
     let successCount = 0;
     let failureCount = 0;
-
     // 分批处理歌曲
     for (let i = 0; i < musicList.length; i += batchSize) {
         const batch = musicList.slice(i, i + batchSize);
@@ -111,12 +71,11 @@ async function netease2audio(musicList, batchSize = 300, delayBetweenBatches = 2
                 // 多次尝试获取歌曲信息
                 while (retryCount < maxRetry) {
                     try {
-                        var URL = await getMusicInfo(music.id);
                         const songInfo = {
-                            name: music.title,
-                            artist: music.artist,
-                            url: URL.url_info.url,
-                            cover: music.cover,
+                            name: music.name,
+                            artist: music.artist + " | " + extractIdFromUrl(music.url),
+                            url: GetMusicLink(extractIdFromUrl(music.url)),
+                            cover: music.pic,
                             lrc: music.lrc
                         };
 
@@ -164,7 +123,7 @@ async function GetAudioList(musicList) {
 
 function extractIdFromUrl(url) {
     try {
-        const urlObject = new URL(url);
+        const urlObject = new URL(url.replace("/#/",'/'));
         const params = new URLSearchParams(urlObject.search);
         const id = params.get('id');
 
