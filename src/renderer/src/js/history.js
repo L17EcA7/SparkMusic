5bb389ef3d017357eed352affcb60c5753049439
im华喵 <littttt87@hotmail.com>
2024-02-07 01:06:05 +0800

    format code

--- src/renderer/src/js/history.js
+++ src/renderer/src/js/history.js
@@ -1,11 +1,11 @@
-const HistoryMaxLength = 30;
+const HistoryMaxLength = 30
 /**
  * 获取歌单历史记录
  * @returns {Array} 歌单历史记录数组
  */
 function getSonglistHistory() {
-    const songlistHistoryStr = localStorage.getItem("songlistHistory");
-    return songlistHistoryStr ? JSON.parse(songlistHistoryStr) : [];
+  const songlistHistoryStr = localStorage.getItem('songlistHistory')
+  return songlistHistoryStr ? JSON.parse(songlistHistoryStr) : []
 }
 
 /**
@@ -14,29 +14,29 @@ function getSonglistHistory() {
  * @param {number} newCount - 新歌单的计数
  */
 function saveSonglistHistory(newId, newCount) {
-    var songlistHistory = getSonglistHistory();
+  var songlistHistory = getSonglistHistory()
 
-    // 检测是否已存在相同的 ID
-    const existingIndex = songlistHistory.findIndex(item => item.id === newId);
+  // 检测是否已存在相同的 ID
+  const existingIndex = songlistHistory.findIndex((item) => item.id === newId)
 
-    if (existingIndex !== -1) {
-        // 如果存在相同的 ID，更新计数
-        songlistHistory[existingIndex].count = newCount;
-    } else {
-        // 如果不存在相同的 ID，添加新的记录
+  if (existingIndex !== -1) {
+    // 如果存在相同的 ID，更新计数
+    songlistHistory[existingIndex].count = newCount
+  } else {
+    // 如果不存在相同的 ID，添加新的记录
 
-        // 检查是否超过 100 条历史记录
-        if (songlistHistory.length >= HistoryMaxLength) {
-            // 移除最旧的记录
-            songlistHistory.shift();
-        }
-
-        // 添加新的记录到数组末尾
-        songlistHistory.push({ id: newId, count: newCount });
+    // 检查是否超过 100 条历史记录
+    if (songlistHistory.length >= HistoryMaxLength) {
+      // 移除最旧的记录
+      songlistHistory.shift()
     }
 
-    // 将更新后的数组保存到本地存储
-    localStorage.setItem("songlistHistory", JSON.stringify(songlistHistory));
+    // 添加新的记录到数组末尾
+    songlistHistory.push({ id: newId, count: newCount })
+  }
+
+  // 将更新后的数组保存到本地存储
+  localStorage.setItem('songlistHistory', JSON.stringify(songlistHistory))
 }
 
 /**
@@ -44,8 +44,8 @@ function saveSonglistHistory(newId, newCount) {
  * @returns {Array} 歌曲历史记录数组
  */
 function getSongHistory() {
-    const songHistoryStr = localStorage.getItem("songHistory");
-    return songHistoryStr ? JSON.parse(songHistoryStr) : [];
+  const songHistoryStr = localStorage.getItem('songHistory')
+  return songHistoryStr ? JSON.parse(songHistoryStr) : []
 }
 
 /**
@@ -54,26 +54,26 @@ function getSongHistory() {
  * @param {string} newName - 新歌曲的名称
  */
 function saveSongHistory(newId, newName) {
-    var songHistory = getSongHistory();
+  var songHistory = getSongHistory()
 
-    // 检测是否已存在相同的 ID
-    const existingIndex = songHistory.findIndex(item => item.id === newId);
+  // 检测是否已存在相同的 ID
+  const existingIndex = songHistory.findIndex((item) => item.id === newId)
 
-    if (existingIndex === -1) {
-        // 如果不存在相同的 ID，添加新的记录
+  if (existingIndex === -1) {
+    // 如果不存在相同的 ID，添加新的记录
 
-        // 检查是否超过 100 条历史记录
-        if (songHistory.length >= HistoryMaxLength) {
-            // 移除最旧的记录
-            songHistory.shift();
-        }
-
-        // 添加新的记录到数组末尾
-        songHistory.push({ id: newId, Name: newName });
-
-        // 将更新后的数组保存到本地存储
-        localStorage.setItem("songHistory", JSON.stringify(songHistory));
+    // 检查是否超过 100 条历史记录
+    if (songHistory.length >= HistoryMaxLength) {
+      // 移除最旧的记录
+      songHistory.shift()
     }
+
+    // 添加新的记录到数组末尾
+    songHistory.push({ id: newId, Name: newName })
+
+    // 将更新后的数组保存到本地存储
+    localStorage.setItem('songHistory', JSON.stringify(songHistory))
+  }
 }
 
-export { getSonglistHistory, saveSonglistHistory, saveSongHistory, getSongHistory };
+export { getSonglistHistory, saveSonglistHistory, saveSongHistory, getSongHistory }
--- src/renderer/src/js/music.js
+++ src/renderer/src/js/music.js
@@ -1,149 +1,147 @@
-import CryptoJS from 'crypto-js';
-import axios from "axios";
+import CryptoJS from 'crypto-js'
+import axios from 'axios'
 const handletoken = (text) => {
-    const now = new Date();
-    const year = now.getFullYear().toString();
-    const month = String(now.getMonth() + 1).padStart(2, '0');
-    const day = String(now.getDate()).padStart(2, '0');
-    const hour = String(now.getHours()).padStart(2, '0');
-    const minute = String(now.getMinutes()).padStart(2, '0');
-    const formattedDateTime = `${year}${month}${day}${text}`;
-    return formattedDateTime;
-};
+  const now = new Date()
+  const year = now.getFullYear().toString()
+  const month = String(now.getMonth() + 1).padStart(2, '0')
+  const day = String(now.getDate()).padStart(2, '0')
+  const hour = String(now.getHours()).padStart(2, '0')
+  const minute = String(now.getMinutes()).padStart(2, '0')
+  const formattedDateTime = `${year}${month}${day}${text}`
+  return formattedDateTime
+}
 const gettoken = (id, level) => {
-
-    const rawToken = id + level + 'urlv1';
-    console.log(rawToken);
-    const encryptedToken = CryptoJS.MD5(rawToken).toString();
-    console.log(encryptedToken);
-    return encryptedToken;
-};
+  const rawToken = id + level + 'urlv1'
+  console.log(rawToken)
+  const encryptedToken = CryptoJS.MD5(rawToken).toString()
+  console.log(encryptedToken)
+  return encryptedToken
+}
 async function _getMusicInfo(ID) {
-    try {
-        const Response = await axios.get(`https://api.injahow.cn/meting/?type=song&id=${ID}`);
-        // 只有在备用接口状态码为200时才返回备用数据
-        if (Response.data.code !== 500) {
-            return Response.data;
-        } else {
-            console.error(`Error fetching playlist info. Status code: ${Response.data.code}`);
-        }
-    } catch (backupError) {
-        console.error('Error fetching playlist info:', backupError);
-        throw new Error('Failed to fetch playlist info');
+  try {
+    const Response = await axios.get(`https://api.injahow.cn/meting/?type=song&id=${ID}`)
+    // 只有在备用接口状态码为200时才返回备用数据
+    if (Response.data.code !== 500) {
+      return Response.data
+    } else {
+      console.error(`Error fetching playlist info. Status code: ${Response.data.code}`)
     }
+  } catch (backupError) {
+    console.error('Error fetching playlist info:', backupError)
+    throw new Error('Failed to fetch playlist info')
+  }
 }
 async function _getPlaylistInfo(ID) {
-    try {
-        const Response = await axios.get(`https://api.injahow.cn/meting/?type=playlist&id=${ID}`);
-        // 只有在备用接口状态码为200时才返回备用数据
-        if (Response.data.code !== 500) {
-            return Response.data;
-        } else {
-            console.error(`Error fetching playlist info. Status code: ${Response.data.code}`);
-        }
-    } catch (backupError) {
-        console.error('Error fetching playlist info:', backupError);
-        throw new Error('Failed to fetch playlist info');
+  try {
+    const Response = await axios.get(`https://api.injahow.cn/meting/?type=playlist&id=${ID}`)
+    // 只有在备用接口状态码为200时才返回备用数据
+    if (Response.data.code !== 500) {
+      return Response.data
+    } else {
+      console.error(`Error fetching playlist info. Status code: ${Response.data.code}`)
     }
+  } catch (backupError) {
+    console.error('Error fetching playlist info:', backupError)
+    throw new Error('Failed to fetch playlist info')
+  }
 }
 
 async function getMusicInfo(ID) {
-    return await _getMusicInfo(ID);
+  return await _getMusicInfo(ID)
 }
 async function getPlaylistInfo(ID) {
-    return await _getPlaylistInfo(ID);
+  return await _getPlaylistInfo(ID)
 }
 function GetMusicLink(ID) {
-    var LEVEL = "jymaster"
-    var TOKEN = gettoken(ID, LEVEL)
-    const now = new Date();
-    console.log(now);
-    return `https://api.toubiec.cn/api/wyapi_v1.php?id=${ID}&type=urlv1&level=${LEVEL}&cont=1&token=${TOKEN}&s=${now}`;
+  var LEVEL = 'jymaster'
+  var TOKEN = gettoken(ID, LEVEL)
+  const now = new Date()
+  console.log(now)
+  return `https://api.toubiec.cn/api/wyapi_v1.php?id=${ID}&type=urlv1&level=${LEVEL}&cont=1&token=${TOKEN}&s=${now}`
 }
 // 示例用法
 
 async function netease2audio(musicList, batchSize = 300, delayBetweenBatches = 20, maxRetry = 3) {
-    const result = [];
-    let successCount = 0;
-    let failureCount = 0;
-    // 分批处理歌曲
-    for (let i = 0; i < musicList.length; i += batchSize) {
-        const batch = musicList.slice(i, i + batchSize);
+  const result = []
+  let successCount = 0
+  let failureCount = 0
+  // 分批处理歌曲
+  for (let i = 0; i < musicList.length; i += batchSize) {
+    const batch = musicList.slice(i, i + batchSize)
 
-        // 使用 Promise.all 来等待当前批次的 Promise 解决
-        const batchResult = await Promise.all(
-            batch.map(async music => {
-                let retryCount = 0;
+    // 使用 Promise.all 来等待当前批次的 Promise 解决
+    const batchResult = await Promise.all(
+      batch.map(async (music) => {
+        let retryCount = 0
 
-                // 多次尝试获取歌曲信息
-                while (retryCount < maxRetry) {
-                    try {
-                        const songInfo = {
-                            name: music.name,
-                            artist: music.artist + " | " + extractIdFromUrl(music.url),
-                            url: GetMusicLink(extractIdFromUrl(music.url)),
-                            cover: music.pic,
-                            lrc: music.lrc
-                        };
+        // 多次尝试获取歌曲信息
+        while (retryCount < maxRetry) {
+          try {
+            const songInfo = {
+              name: music.name,
+              artist: music.artist + ' | ' + extractIdFromUrl(music.url),
+              url: GetMusicLink(extractIdFromUrl(music.url)),
+              cover: music.pic,
+              lrc: music.lrc
+            }
 
-                        // 在这里添加检查逻辑，如果 songInfo 不符合条件，则置为 null
-                        if (!(songInfo && typeof songInfo === 'object' && songInfo.url !== undefined)) {
-                            console.error(`歌曲信息获取失败，放弃添加歌曲:`, music);
-                            failureCount++;
-                            break; // 如果失败，退出重试循环
-                        }
+            // 在这里添加检查逻辑，如果 songInfo 不符合条件，则置为 null
+            if (!(songInfo && typeof songInfo === 'object' && songInfo.url !== undefined)) {
+              console.error(`歌曲信息获取失败，放弃添加歌曲:`, music)
+              failureCount++
+              break // 如果失败，退出重试循环
+            }
 
-                        successCount++;
-                        return songInfo; // 如果成功获取歌曲信息，返回
-                    } catch (error) {
-                        console.error(`获取歌曲信息失败（${retryCount + 1}/${maxRetry}）:`, error);
-                        retryCount++;
-                    }
-                }
-
-                failureCount++;
-                //return null; // 如果多次重试后仍然无法获取歌曲信息，返回 null
-            })
-        );
-
-        // 将当前批次的结果添加到总结果中
-        const validBatchResult = batchResult.filter(songInfo => songInfo !== null);
-        result.push(...validBatchResult);
-
-        // 在处理下一批之前等待一段时间
-        if (i + batchSize < musicList.length) {
-            await new Promise(resolve => setTimeout(resolve, delayBetweenBatches));
+            successCount++
+            return songInfo // 如果成功获取歌曲信息，返回
+          } catch (error) {
+            console.error(`获取歌曲信息失败（${retryCount + 1}/${maxRetry}）:`, error)
+            retryCount++
+          }
         }
+
+        failureCount++
+        //return null; // 如果多次重试后仍然无法获取歌曲信息，返回 null
+      })
+    )
+
+    // 将当前批次的结果添加到总结果中
+    const validBatchResult = batchResult.filter((songInfo) => songInfo !== null)
+    result.push(...validBatchResult)
+
+    // 在处理下一批之前等待一段时间
+    if (i + batchSize < musicList.length) {
+      await new Promise((resolve) => setTimeout(resolve, delayBetweenBatches))
     }
+  }
 
-    const finalResult = { result, successCount, failureCount };
-    console.log(finalResult); // 添加这行用于调试
+  const finalResult = { result, successCount, failureCount }
+  console.log(finalResult) // 添加这行用于调试
 
-    return { result, successCount, failureCount };
+  return { result, successCount, failureCount }
 }
 
 async function GetAudioList(musicList) {
-    const result = await netease2audio(musicList);
-    console.log(result);
-    return result;
+  const result = await netease2audio(musicList)
+  console.log(result)
+  return result
 }
 
 function extractIdFromUrl(url) {
-    try {
-        const urlObject = new URL(url.replace("/#/", '/'));
-        const params = new URLSearchParams(urlObject.search);
-        const id = params.get('id');
+  try {
+    const urlObject = new URL(url.replace('/#/', '/'))
+    const params = new URLSearchParams(urlObject.search)
+    const id = params.get('id')
 
-        if (id) {
-            return id;
-        } else {
-            throw new Error('ID parameter not found in the URL.');
-        }
-    } catch (error) {
-        console.error('Error extracting ID from URL:', error.message);
-        throw error;
+    if (id) {
+      return id
+    } else {
+      throw new Error('ID parameter not found in the URL.')
     }
+  } catch (error) {
+    console.error('Error extracting ID from URL:', error.message)
+    throw error
+  }
 }
-
 
 export { getMusicInfo, getPlaylistInfo, GetMusicLink, extractIdFromUrl, GetAudioList }
